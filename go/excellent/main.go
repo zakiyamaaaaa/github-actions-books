caa8ea51516266c55927934a68a371f4165f9f0e
zakiyamaaaaa <s.zakiyama@gmail.com>
2025-01-02 12:22:23 +0900

    no message

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
+var version string
+
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
 	} else {
 		return "odd"
 	}
+}
+
+func main() {
+	fmt.Printf("Version: %s\n", version)
 }
